6bb428187966f8e743efd97af7863c8e5d63415a
Maksim Nosov <ozolc@yandex.ru>
2020-01-04 18:36:07 +0300

    Day 41 – Project 8, part three

---
+++ 41/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Maksim Nosov on 03.01.2020.
+//  Copyright © 2020 Maksim Nosov. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
